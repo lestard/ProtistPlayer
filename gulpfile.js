988048a3b7ab9b9a5bf2212fe5a070289cbfb08d
Manuel Mauky <manuel.mauky@googlemail.com>
2014-07-02 17:22:26 +0200

    Use gulp as build tool together with node.js

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+
+var shell = require("gulp-shell");
+
+
+gulp.task("run-ff", function(){
+   console.log("Running the application with Firefox");
+    return gulp.src("")
+        .pipe(shell([
+            'firefox -app src/application.ini'
+        ]));
+});
+
+
+gulp.task("run-xr", function(){
+   console.log("Running the application with XULRunner");
+    return gulp.src("")
+        .pipe(shell([
+            'xulrunner src/application.ini'
+        ]));
+});
